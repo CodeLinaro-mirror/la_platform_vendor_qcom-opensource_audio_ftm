85421b290d20c253db7277041f91cbe0d46dd0fd
Ravulapati Vishnu Vardhan Rao <visr@qti.qualcomm.com>
2025-05-22 15:34:35 +0530

    audio_ftm: Alor: update Makefile for ftm for Alor variants
    Change-Id: Ie4c085db2c48309453179455f93193a35c9ae049
    Signed-off-by: Ravulapati Vishnu Vardhan Rao <visr@qti.qualcomm.com>

--- ar/Android.mk
+++ ar/Android.mk
@@ -375,6 +375,36 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
 LOCAL_SRC_FILES    := config/canoe/ftm_test_config_canoe-qrd-wsa884x-snd-card
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ftm_test_config_alor-mtp-wcd9378-snd-card
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_OWNER := qti
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
+LOCAL_SRC_FILES    := config/canoe/ftm_test_config_alor-mtp-wcd9378-snd-card
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ftm_test_config_alor-mtp-wcd939x-snd-card
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_OWNER := qti
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
+LOCAL_SRC_FILES    := config/canoe/ftm_test_config_alor-mtp-wcd939x-snd-card
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ftm_test_config_alor-qrd-snd-card
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_OWNER := qti
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
+LOCAL_SRC_FILES    := config/canoe/ftm_test_config_alor-qrd-snd-card
+include $(BUILD_PREBUILT)
 endif
 
 ifeq ($(call is-board-platform-in-list,monaco),true)
