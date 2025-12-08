48d5248b32d442d24f5cd840a5841777cfd6707c
Siddaram Vannal <vannal@qti.qualcomm.com>
2025-12-07 17:31:10 -0800

    Revert "audio_ftm: chora: add ftm config files for chora"
    This reverts commit d953c127378f67bf90e6a0318c3d4e58f1cb8598.
    
    Reason for revert: Causing audio_ftm issue.
    
    Change-Id: Iaee11f15505b279ce2de7928fae3632d6a659497

--- ar/Android.mk
+++ ar/Android.mk
@@ -377,36 +377,6 @@ LOCAL_SRC_FILES    := config/canoe/ftm_test_config_canoe-qrd-wsa884x-snd-card
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := ftm_test_config
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_OWNER := qti
-LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
-LOCAL_SRC_FILES    := config/chora/ftm_test_config
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ftm_test_config_chora-mtp-qmp-snd-card
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_OWNER := qti
-LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
-LOCAL_SRC_FILES    := config/chora/ftm_test_config_chora-mtp-qmp-snd-card
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ftm_test_config_chora-qrd-snd-card
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_OWNER := qti
-LOCAL_VENDOR_MODULE := true
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
-LOCAL_SRC_FILES    := config/chora/ftm_test_config_chora-qrd-snd-card
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ftm_test_config_alor-mtp-wcd9378-snd-card
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
