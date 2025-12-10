d69a6b1f59dc7ddf4234eb7f494fcce12c6ce395
Sairam Peri <peri@qti.qualcomm.com>
2025-12-10 13:04:17 +0530

    audio_ftm: malabar: add support for malabar variants
    Add audio_ftm support for malabar variants.
    
    Change-Id: Ibd07c4492ec38218585a33885157554790c30734

--- ar/Android.mk
+++ ar/Android.mk
@@ -440,6 +440,29 @@ LOCAL_SRC_FILES    := config/chora/ftm_test_config_chora-qrd-snd-card
 include $(BUILD_PREBUILT)
 endif
 
+ifeq ($(call is-board-platform-in-list,malabar),true)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ftm_test_config
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_OWNER := qti
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
+LOCAL_SRC_FILES    := config/malabar/ftm_test_config
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ftm_test_config_malabar-qrd-snd-card
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_OWNER := qti
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/
+LOCAL_SRC_FILES    := config/malabar/ftm_test_config_malabar-qrd-snd-card
+include $(BUILD_PREBUILT)
+endif
+
 ifeq ($(call is-board-platform-in-list,monaco),true)
 ifeq ($(TARGET_SUPPORTS_WEAR_AON),true)
  include $(CLEAR_VARS)
